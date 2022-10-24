477b9cdd8b6af6b0e8f4c73201bd69e41604f375
Maxim-Davydov <116034877+Maxim-Davydov@users.noreply.github.com>
2022-10-24 11:06:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int a = n / 100;
+int c = (n / 10) % 10;
+int b = n % 10;
+int x = 100 * a + 10 * b + c;
+Console.WriteLine(x);
+Console.ReadLine();
